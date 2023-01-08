ad554c32f23429bc5d76b09cac08d425b2294dde
Vishwaa Sofat <vishwaa@berkeley.edu>
2023-01-08 02:26:42 -0500

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 export const GOOGLE_CLIENT_ID =
   process.env.NEXT_APP_GOOGLE_CLIENT_ID ||
-  "665058591652-24kl39va132h027284rpdprhstt0at7d.apps.googleusercontent.com";
+  "432468212991-rv1gecbee82aiiugi4vtl7tru5dqrtp0.apps.googleusercontent.com";
 
 export const PUBLIC_URL =
   process.env.PUBLIC_URL || process.env.NODE_ENV === "production"
